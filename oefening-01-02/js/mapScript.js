f44d015a7ba3a1bf1729ff442b90115d284f8083
Thanh-81 <chiemthanhhuynh@gmail.com>
2024-05-11 22:11:13 +0200

    week 9
    labo 9

--- oefening-01-02/js/mapScript.js
+++ oefening-01-02/js/mapScript.js
@@ -3,6 +3,24 @@
 * Verander in de code online alle "var" in "let".
 */
 
+/*
+ - AP-Hogeschool ligt in de Ellermanstraat 33 op de coordinaten `[51.23009, 4.41616]`. Zet op deze plek een `marker`, zoom in op stand `17`.
+ - Zorg ervoor dat de campus Ellerman wordt aangeduid met een *rood vierkant* op de kaart.
+   **tip:** lees https://leafletjs.com/reference.html#rectangle  
+    gebruik als bounds voor de rechthoek: `[51.23041, 4.4155]` en `[51.22991, 4.41675]`
+*/
+
+
+let map = L.map('apMap').setView([51.23009, 4.41616], 17);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+
+let polygon = L.rectangle([[51.23041, 4.4155], [51.22991, 4.41675]], { color: 'red', fillColor: 'red', fillOpacity: 0.5 }).addTo(map);
+
+let marker = L.marker([51.23009, 4.41616]).addTo(map); marker.bindPopup("<b>AP-Hogeschool</b><br>Ellermanstraat 33").openPopup();
 
 
 
